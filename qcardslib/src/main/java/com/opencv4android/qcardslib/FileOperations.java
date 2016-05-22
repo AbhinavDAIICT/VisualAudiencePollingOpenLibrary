43d135d7e5e0b9bb69d280fd151017dd9fe48086
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2016-05-22 14:01:10 +0530

    reading predefined idMap to establish cardID -> studentId relation

---
+++ qcardslib/src/main/java/com/opencv4android/qcardslib/FileOperations.java
@@ -0,0 +1,75 @@
+package com.opencv4android.qcardslib;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.StringTokenizer;
+
+/**
+ * Created by abhinav tripathi on 28/2/16.
+ */
+public class FileOperations {
+
+    public static LinkedHashMap<String, MappedObjects> readIdMap(Context context){
+        LinkedHashMap<String, MappedObjects> idMap = new LinkedHashMap<>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open("idMap.csv")));
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                MappedObjects mappedObjects = new MappedObjects();
+                int count = 0;
+                String detectedId = null;
+                StringTokenizer stringTokenizer = new StringTokenizer(mLine,",");
+                while(stringTokenizer.hasMoreTokens()){
+                    switch(count){
+                        case 0:
+                            detectedId = stringTokenizer.nextToken();
+                            mappedObjects.setDetectedId(detectedId);
+                            count++;
+                            break;
+                        case 1:
+                            mappedObjects.setMappedId(stringTokenizer.nextToken());
+                            count++;
+                            break;
+                        case 2:
+                            mappedObjects.setMappedW(stringTokenizer.nextToken());
+                            count++;
+                            break;
+                        case 3:
+                            mappedObjects.setMappedX(stringTokenizer.nextToken());
+                            count++;
+                            break;
+                        case 4:
+                            mappedObjects.setMappedY(stringTokenizer.nextToken());
+                            count++;
+                            break;
+                        case 5:
+                            mappedObjects.setMappedZ(stringTokenizer.nextToken());
+                            count=0;
+                            break;
+                        default:
+                            count = 0;
+                            break;
+                    }
+                    idMap.put(detectedId,mappedObjects);
+                }
+            }
+        } catch (IOException e) {
+            //log the exception
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+        return idMap;
+    }
+}
